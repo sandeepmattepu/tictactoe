c97ff4a28e41ac919bdea763c3c51231ff5bb5ca
Sandeep Yadav Mattepu <sandeepyadavmattepu@Sandeeps-MBP.local>
2017-10-29 23:28:14 +0100

    Linear function added

---
+++ src/com/sandeep/machine/learning/lms/LinearEquation.java
@@ -0,0 +1,178 @@
+package com.sandeep.machine.learning.lms;
+
+/**
+ * This class acts as linear equation which has co-efficient and variable.
+ * Example : w0 + (w1 x X1) + (w2 x X2) here w0, w1, w2 are co-efficients and X1,X2,X3 are variables
+ * */
+public class LinearEquation 
+{
+	private float[] coefficientValues;
+	private float[] variableValues;
+	
+	/**
+	 * Creates an instance of linear equation
+	 * @param numberOfVariables Enter the number of variable function.
+	 * For example : w0 + (w1 x X1) here value is 1
+	 * */
+	public LinearEquation(int numberOfVariables)
+	{
+		coefficientValues = new float[numberOfVariables + 1];
+		variableValues = new float[numberOfVariables];
+	}
+	
+	/**
+	 * This function helps us to access individual values
+	 * @param at Enter number as index value similar to arrays
+	 * @return Co-efficient value at the "at" index
+	 * */
+	public float getCoefficientAt(int at)
+	{
+		if(at < 0)
+		{
+			return coefficientValues[0];
+		}
+		else if(at >= coefficientValues.length)
+		{
+			return coefficientValues[coefficientValues.length - 1];
+		}
+		else
+		{
+			return coefficientValues[at];
+		}
+	}
+	
+	/**
+	 * This function helps us to set individual coefficients
+	 * @param at Enter number as index value similar to arrays
+	 * @param as Enter coefficient value to be assigned
+	 * */
+	public void setCoefficientAt(int at, float as)
+	{
+		if(at < 0 || at >= coefficientValues.length)
+		{
+			return;
+		}
+		
+		coefficientValues[at] = as;
+	}
+	
+	/**
+	 * This function return all values of the coefficients
+	 * @return Returns {@link com.sandeep.machine.learning.lms.LinearEquation#coefficientValues}
+	 * */
+	public float[] getAllCoefficients()
+	{
+		return coefficientValues;
+	}
+	
+	/**
+	 * This function gives number of degrees of equation.
+	 * @return Number of degrees of equation. Example : w0 + (w1 x X1) here value returned is 1
+	 * */
+	public int degreeOfEquation()
+	{
+		return variableValues.length;
+	}
+	
+	/**
+	 * This function gives total value of the function with values which was there previously in them.
+	 * @return Value of the function.
+	 * */
+	public float valueOfFunction()
+	{
+		float result = 0;
+		if(variableValues.length == 0)
+		{
+			return result;
+		}
+		else
+		{
+			result += coefficientValues[0];
+			for(int i = 0; i < variableValues.length; i++)
+			{
+				result += (coefficientValues[i+1] * variableValues[i]);
+			}
+			return result;
+		}
+	}
+	
+	/**
+	 * This function gives total value of the function using values present in variables
+	 * @param allVariableValues Enter array which has variable values in them.
+	 * Example : w0 + (w1 * X1) then send {X1 = 9} array
+	 * @return Value of the function.
+	 * */
+	public float valueOfFunction(float[] allVariableValues)
+	{
+		float result = 0;
+		if(variableValues.length == 0)
+		{
+			return result;
+		}
+		else
+		{
+			int j = variableValues.length;
+			if(variableValues.length > allVariableValues.length)
+			{
+				j = allVariableValues.length;
+			}
+			for(int i = 0; i < j; i++)
+			{
+				variableValues[i] = allVariableValues[i];
+			}
+			
+			result += coefficientValues[0];
+			for(int i = 0; i < variableValues.length; i++)
+			{
+				result += (coefficientValues[i+1] * variableValues[i]);
+			}
+			return result;
+		}
+	}
+	
+	/**
+	 * This function gives total value of the function using values present in variables and coefficients
+	 * @param allVariableValues Enter array which has variable values in them.
+	 * Example : w0 + (w1 * X1) then send {X1 = 9} array.
+	 * @param coEffValues Enter array which has co-efficient values in them.
+	 * Example : w0 + (w1 * X1) then send {w0 = 0.34, w1 = 0.645} array.
+	 * @return Value of the function.
+	 * */
+	public float valueOfFunction(float[] allVariableValues, float[] coEffValues)
+	{
+		float result = 0;
+		if(variableValues.length == 0)
+		{
+			return result;
+		}
+		else
+		{
+			int j = variableValues.length;
+			if(variableValues.length > allVariableValues.length)
+			{
+				j = allVariableValues.length;
+			}
+			for(int i = 0; i < j; i++)
+			{
+				variableValues[i] = allVariableValues[i];
+			}
+			
+			int k = coefficientValues.length;
+			if(coefficientValues.length > coEffValues.length)
+			{
+				k = coEffValues.length;
+			}
+			for(int i = 0; i < k; i++)
+			{
+				coefficientValues[i] = coEffValues[i];
+			}
+			
+			result += coefficientValues[0];
+			for(int i = 0; i < variableValues.length; i++)
+			{
+				result += (coefficientValues[i+1] * variableValues[i]);
+			}
+			return result;
+		}
+	}
+}
