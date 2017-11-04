99d08d3ce245e858ccfe4f6ec2a44e51af02eb04
Sandeep Yadav Mattepu <sandeepyadavmattepu@Sandeeps-MBP.local>
2017-11-04 02:07:10 +0100

    Games are played as tournament

--- src/com/sandeep/solutions/task1/SMCustomMatch.java
+++ src/com/sandeep/solutions/task1/SMCustomMatch.java
@@ -1,25 +1,26 @@
 package com.sandeep.solutions.task1;
 
+import java.util.HashSet;
+import java.util.Set;
 import de.ovgu.dke.teaching.ml.tictactoe.api.IBoard;
 import de.ovgu.dke.teaching.ml.tictactoe.api.IPlayer;
 import de.ovgu.dke.teaching.ml.tictactoe.game.Board3D;
-import de.ovgu.dke.teaching.ml.tictactoe.game.Match;
+import de.ovgu.dke.teaching.ml.tictactoe.game.Tournament;
 import de.ovgu.dke.teaching.ml.tictactoe.player.RandomPlayer;
 
 public class SMCustomMatch
 {
-	private static int numberOfMatches = 25;
+	private static int numberOfMatches = 10;
 	public static void main(String[] args)
 	{
 		IBoard simpleBoard = new Board3D(5);
 		IPlayer learningPlayer = new SolutionLearningPlayer();
 		IPlayer randomPlayer = new RandomPlayer();
-		Match match;
-		for(int i = 1; i <= numberOfMatches; i++)
-		{
-			match = new Match(simpleBoard, learningPlayer, randomPlayer);
-			match.play();
-			simpleBoard = new Board3D(5);
-		}
+		Set<IPlayer> players = new HashSet<IPlayer>();
+		players.add(randomPlayer);
+		players.add(learningPlayer);
+		Tournament smallTournament = new Tournament(simpleBoard, players, numberOfMatches);
+		smallTournament.run();
+
 	}
 }
