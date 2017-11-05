c95c4f825ed9b393106f372630167e8ba1087f7a
Sandeep Yadav Mattepu <sandeepyadavmattepu@Sandeeps-MBP.local>
2017-11-05 21:23:52 +0100

    Tournament removed and iterative games added

--- src/com/sandeep/solutions/task1/SMCustomMatch.java
+++ src/com/sandeep/solutions/task1/SMCustomMatch.java
@@ -5,24 +5,63 @@ import java.util.Set;
 import de.ovgu.dke.teaching.ml.tictactoe.api.IBoard;
 import de.ovgu.dke.teaching.ml.tictactoe.api.IPlayer;
 import de.ovgu.dke.teaching.ml.tictactoe.game.Board3D;
+import de.ovgu.dke.teaching.ml.tictactoe.game.Match;
 import de.ovgu.dke.teaching.ml.tictactoe.game.Tournament;
 import de.ovgu.dke.teaching.ml.tictactoe.player.RandomPlayer;
 
 public class SMCustomMatch
 {
 	private static int numberOfMatches = 10;
+	private static IBoard board;
+	private static Match match;
+	private static IPlayer learningPlayer;
+	private static IPlayer randomPlayer;
+	private static int numberOfDraws = 0;
+	private static int numberOfLoses = 0;
+	private static int numberOfWins = 0;
+	
 	public static void main(String[] args)
 	{
 		analyzeCommandLineArguments(args);
-		IBoard simpleBoard = new Board3D(5);
-		IPlayer learningPlayer = new SolutionLearningPlayer();
-		IPlayer randomPlayer = new RandomPlayer();
-		Set<IPlayer> players = new HashSet<IPlayer>();
-		players.add(randomPlayer);
-		players.add(learningPlayer);
-		Tournament smallTournament = new Tournament(simpleBoard, players, numberOfMatches);
-		smallTournament.run();
-
+		learningPlayer = new SolutionLearningPlayer();
+		randomPlayer = new RandomPlayer();
+		playGames();
+		displayResults();
+	}
+	
+	private static void playGames()
+	{
+		for(int i = 1; i <= numberOfMatches; i++)
+		{
+			board = new Board3D(5);
+			match = new Match(board, learningPlayer, randomPlayer);
+			match.play();
+			updateGameWinLoseDraw(match);
+		}
+	}
+	
+	private static void displayResults()
+	{
+		System.out.println(learningPlayer.getName() + " stats:");
+		System.out.println("Wins : " + numberOfWins);
+		System.out.println("Loses : " + numberOfLoses);
+		System.out.println("Draws : " + numberOfDraws);
+	}
+	
+	private static void updateGameWinLoseDraw(Match matchToAnalyze)
+	{
+		if(matchToAnalyze.getWinner() == null)
+		{
+			numberOfDraws++;
+		}
+		else if(matchToAnalyze.getWinner() == learningPlayer)
+		{
+			numberOfWins++;
+		}
+		else
+		{
+			numberOfLoses++;
+		}
 	}
 	
 	private static void analyzeCommandLineArguments(String[] args)
